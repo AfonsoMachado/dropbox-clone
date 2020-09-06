2c8ecbc278cca8b34bf76fac21cae3428144f09b
Afonso Machado <afonso777@hotmail.com>
2020-09-05 21:39:09 -0300

    criando metodos js para exibir ou nao o menu lateral

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from './styles';
 
+const scrollThreshold = 300;
+
 const SideMenu: React.FC = ({ children }) => {
+  const [scrollY, setScrollY] = useState(0);
+  // para exibir o menu lateral no clique
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    function onScroll() {
+      setScrollY(window.scrollY)
+      // quando o usuario usar o scroll, o painel lateral é oculto
+      setIsActive(false);
+    }
+
+    // registrando um evento de scroll 
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, []);
+
+  // para representar os modos de operação do menu lateral
+  const classes = [
+    isActive ? 'open' : '',
+    scrollY <= scrollThreshold ? 'scrollOpen' : '',
+  ];
+
+  const className = classes.join(' ').trim();
+
   return (
-    <Container>
+    <Container className={className} >
       {children}
     </Container>
 
